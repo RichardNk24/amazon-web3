60a18a2f925717520c16bdeb87d09f4305b0dd45
Richard Channing <richardsobelnkulu@gmail.com>
2022-05-06 13:07:54 +0200

    Updating the project with components plus multiple components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return ( 
+  <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC}
+  >
+    <Component {...pageProps} />
+  </MoralisProvider>)
 }
 
 export default MyApp
